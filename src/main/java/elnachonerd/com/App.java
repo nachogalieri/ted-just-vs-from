bb78df0b3fa3e2fc08efda10faf77bb585cd024f
nachogalieri <ignacio.galieri@mercadolibre.com.co>
2020-11-20 05:43:31 -0500

    Add pool to main process

--- src/main/java/elnachonerd/com/App.java
+++ src/main/java/elnachonerd/com/App.java
@@ -1,14 +1,23 @@
 package elnachonerd.com;
 
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import elnachonerd.com.hidden.UseCase;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import java.util.concurrent.ExecutorService;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
 
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
         final UseCase useCase = new UseCase();
         long startTime = System.currentTimeMillis();
-        useCase.useCaseOne().subscribe(
+        ExecutorService pool = newFixedThreadPool(10, new ThreadFactoryBuilder().setNameFormat("nachos-g-%d").build());
+        Scheduler scheduler = Schedulers.from(pool);
+        useCase.useCaseOne().subscribeOn(scheduler).subscribe(
             x -> {
                 long endTime = System.currentTimeMillis();
                 System.out.println("First Time " + (endTime-startTime));
@@ -20,5 +29,6 @@ public class App {
             }
         );
         Thread.sleep(3000);
+        pool.shutdownNow();
     }
 }
